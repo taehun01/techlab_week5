669097f25eb4a3de28c50104bb23fa9c8e3b7b85
신용준 <4rest528@gmail.com>
2026-09-23 20:31:50 +0900

    안쓰는 파일 삭제

--- Source/Editor/Application/FEditorApplication.cpp
+++ Source/Editor/Application/FEditorApplication.cpp
@@ -19,7 +19,6 @@
 
 #include "Runtime/Actors/AActor.h"
 #include "Runtime/Actors/AInstancingActor.h"
-#include "Runtime/Actors/TestTextActor.h"
 #include "Runtime/CoreUObject/UPlaneComp.h"
 #include "Runtime/CoreUObject/USphereComp.h"
 #include "Runtime/Core/FStatRegistry.h"
